9ef8694e67a912d626e61acea3ad2eddef10ae54
David Pérez Orfila <knight_bcn@hotmail.com>
2015-10-07 19:02:38 +0200

    UF1-RA1

---
+++ UF1-RA1/Form2.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n1 As Integer
+        Dim n2 As Integer
+        Dim n3 As Integer
+        Dim menor As Integer
+
+
+        n1 = CInt(Textbox1.Text())
+        n2 = CInt(Textbox2.Text())
+        n3 = CInt(Textbox3.Text())
+
+
+        If n1 < n2 And n1 < n3 Then
+            menor = n1
+        ElseIf n2 < n1 < n3 Then
+            menor = n2
+        Else
+            menor = n3
+
+        End If
+
+        TextBox4.Text = menor.ToString()
+    End Sub
+End Class
