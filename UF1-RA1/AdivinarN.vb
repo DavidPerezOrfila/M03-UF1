d0e3979ff2e605f475dbf740863c0f931df6a6cb
David Pérez Orfila <knight_bcn@hotmail.com>
2015-10-28 19:00:18 +0100

    AcertarN

---
+++ UF1-RA1/AdivinarN.vb
@@ -0,0 +1,30 @@
+﻿Public Class AdivinarN
+    Const NUM As Integer = 7
+    Const INTENTOSMAX As Integer = 6
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer
+        Dim intentos As Integer = 0
+        Dim acertado As Integer = False
+
+        Do
+            n = InputBox("Introduce un número - Te quedan " + (INTENTOSMAX - intentos).ToString() + "intentos")
+            If (n = NUM) Then
+                acertado = True
+                Exit Do
+            End If
+
+            intentos = intentos + 1
+        Loop Until (intentos > INTENTOSMAX)
+
+        If (acertado) Then
+            MessageBox.Show("Acertaste!")
+        Else
+            MessageBox.Show("Has superado el número de intentos máximo!")
+        End If
+
+
+
+
+    End Sub
+End Class
