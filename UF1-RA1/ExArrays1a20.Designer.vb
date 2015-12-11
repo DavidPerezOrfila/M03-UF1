46e24cd45d4d3f8d265126511c18767c54e28ae7
David Pérez Orfila <knight_bcn@hotmail.com>
2015-12-11 19:19:05 +0100

    ExArrays

--- UF1-RA1/ExArrays1a20.Designer.vb
+++ UF1-RA1/ExArrays1a20.Designer.vb
@@ -24,6 +24,16 @@ Partial Class ExArrays1a20
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -44,11 +54,111 @@ Partial Class ExArrays1a20
         Me.Button2.Text = "Ex2Arrays"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(206, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "ArrayMulti"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 157)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Ex6Arrays"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(206, 41)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "ArrayRnd"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 70)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Ex3Arrays"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(12, 99)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Ex4Arrays"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(12, 128)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Ex5Arrays"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 186)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Ex7Arrays"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 215)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Ex8Arrays"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(12, 244)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "Ex9Arrays"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(12, 273)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "Ex10Arrays"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
         'ExArrays1a20
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(337, 342)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Name = "ExArrays1a20"
@@ -59,4 +169,14 @@ Partial Class ExArrays1a20
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
 End Class
--- UF1-RA1/ExArrays1a20.vb
+++ UF1-RA1/ExArrays1a20.vb
@@ -32,4 +32,139 @@
         suma = suma + (vector.ToString + Environment.NewLine)
         MessageBox.Show(suma.ToString)
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim tabla(2, 2) As Integer
+        Dim salida As String = ""
+        Dim i, j As Integer
+        tabla = {
+            {2, 3, 4},
+            {1, 5, 0},
+            {2, 4, 1}
+        }
+        For i = 0 To 2
+            For j = 0 To 2
+                salida = salida + tabla(i, j).ToString + " "
+            Next
+            salida = salida + vbNewLine
+        Next
+        MessageBox.Show(salida)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim tabla(2, 2) As Integer
+        Dim salida As String = ""
+        Dim i, j As Integer
+        Dim r As New Random()
+        tabla = {
+            {2, 3, 4},
+            {1, 5, 0},
+            {2, 4, 1}
+        }
+        For i = 0 To 2
+            For j = 0 To 2
+                If i = j Then 'diagonal
+                    tabla(i, j) = 0
+                End If
+
+            Next
+
+        Next
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim i, j As Integer
+        Dim tabla(2, 2) As Integer
+        Dim r As New Random()
+        For i = 0 To 2
+            For j = 0 To 2
+                If i = j Then
+                    tabla(i, j) = r.Next(0, 100) 'random entre 0 y 100
+                End If
+            Next
+
+        Next
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim tabla(19)
+        Dim cnt As Integer
+        Dim r As New Random()
+        Dim salida As String = ""
+        For cnt = 0 To 19
+            tabla(cnt) = r.Next()
+            If tabla(cnt) > 30 Then
+                salida = salida + tabla(cnt).ToString + " "
+            Else
+                salida = salida + tabla(cnt).ToString + "NO HAY NUMEROS MAYORES DE 30"
+
+            End If
+        Next
+        MessageBox.Show(salida)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim origen() As Integer = {1, 4, 10, 32, 60, 91, 34, 56, 73, 88, 86}
+        Dim destino As Integer
+        Dim cnt As Integer
+        Dim salida As String = " "
+        destino = origen(0)
+
+        For origen(cnt) = 0 To 10
+            If origen(cnt) > 25 Mod 2 = 0 Then
+                destino = origen(cnt)
+                salida = salida + (origen(cnt).ToString + " " + destino.ToString())
+
+            End If
+            MessageBox.Show(salida)
+        Next
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim tabla(9, 9) As Integer
+        Dim i, j As Integer
+        Dim r As New Random()
+        Dim salida As String = ""
+
+        For i = 0 To 9
+            For j = 0 To 9
+                tabla(i, j) = r.Next(0, 100)
+            Next
+        Next
+        For i = 0 To 9
+            For j = 0 To 9
+                If i Mod 2 = 0 Then
+                    i = 1
+                Else
+                    i = 0
+                End If
+
+            Next
+            salida = salida + tabla(i, j).ToString + " "
+        Next
+        MessageBox.Show(salida)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim matriz(9, 9) As Integer
+        Dim i, j As Integer
+        Dim r As New Random()
+        Dim salida As String = ""
+        Dim suma As Integer
+        Dim cnt As Integer
+
+        For i = 0 To 9
+            For j = 0 To 9
+                matriz(i, j) = r.Next(0, 100)
+            Next
+        Next
+        For i = 0 To 9
+            For j = 0 To 9
+
+            Next
+        Next
+    End Sub
 End Class
